ed0b84f34aad2211892c96e25cb6610bbbd72c8f
Mahesh61437 <mahesh61437mahe@gmail.com>
2021-05-22 12:49:12 +0530

    simplifying with DP

--- math/attendance.py
+++ math/attendance.py
@@ -1,20 +1,22 @@
-def get_nth_tetranacci(n):
-    adict = {0: 0, 1: 0, 2: 0, 3: 1, 4: 3}
+def get_nth_tetranacci(n, adict):
     if n in adict:
         return adict[n]
 
-    adict[n] = 3*get_nth_tetranacci(n-1) - get_nth_tetranacci(n-2) - get_nth_tetranacci(n-3) - get_nth_tetranacci(n-4) - 2*get_nth_tetranacci(n-5)
+    adict[n] = 3 * get_nth_tetranacci(n - 1, adict) - get_nth_tetranacci(n - 2, adict) \
+               - get_nth_tetranacci(n - 3, adict) - get_nth_tetranacci(n - 4, adict) - 2 * get_nth_tetranacci(n - 5,
+                                                                                                              adict)
 
     return adict[n]
 
 
 if __name__ == '__main__':
-
     N = int(input('Enter number of days:  '))
 
-    total_ways = 2**N
+    adict = {0: 0, 1: 0, 2: 0, 3: 1, 4: 3}
 
-    no_ways = get_nth_tetranacci(N-1)
+    total_ways = 2 ** N
+
+    no_ways = get_nth_tetranacci(N - 1, adict)
 
     print(f"\nnumber of ways to attend == {total_ways - no_ways}")
-    print(f"probability to miss == {no_ways/total_ways}")
+    print(f"probability to miss == {no_ways / total_ways}")
